5ccf45e65ce1f83211018ccf61e2fecc13dc42d6
filipecsoares <filipecarlossoares@gmail.com>
2023-09-28 21:47:57 -0300

    feat: take argument api

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = std::env::args().collect();
+    if args.len() > 1 && args[1] == "-api" {
+        println!("Hello, API!");
+    } else {
+        println!("Hello, CLI!");
+    }
 }
