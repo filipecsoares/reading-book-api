1daaf9a9d67f04148afd615ef9196b166420b139
filipecsoares <filipecarlossoares@gmail.com>
2024-07-14 10:16:44 -0300

    feat: add book cli

--- src/cli/cli.rs
+++ src/cli/cli.rs
@@ -1,14 +1,16 @@
-use crate::db::db;
+use crate::{
+    db::db::{self},
+    model::book::{Book, ReadingStatus},
+};
 
 const DB_FILENAME: &str = ".books.json";
 
 pub fn run() {
     println!("Managing Books, CLI!");
     print_menu();
-    println!("Please enter your option:");
-    let mut input = String::new();
-    std::io::stdin().read_line(&mut input).unwrap();
+    let input = read_input(String::from("Please enter your option:"));
     match input.trim() {
+        "1" => add_book(),
         "2" => view_books(),
         _ => println!("Invalid option"),
     }
@@ -18,6 +20,39 @@ pub fn run() {
 fn view_books() {
     let books = db::read_books(DB_FILENAME).expect("Failed to read books");
     println!("Books: {:?}", books);
+}
+
+fn add_book() {
+    let title = read_input(String::from("Book Title:"));
+    let author = read_input(String::from("Author:"));
+    let isbn = read_input(String::from("ISBN:"));
+    let year: i16 = read_input("Year:".to_string())
+        .parse()
+        .expect("Not a valid year number");
+    let pages: i16 = read_input("Pages:".to_string())
+        .parse()
+        .expect("Not a valid number");
+    let book = Book::new(
+        title,
+        author,
+        isbn,
+        year,
+        pages,
+        Some(ReadingStatus::ToRead),
+        None,
+        None,
+    );
+    let result = db::add_book(book, DB_FILENAME);
+    if result.is_err() {
+        println!("Something went wrong!");
+    }
+}
+
+fn read_input(message: String) -> String {
+    println!("{}", message);
+    let mut input = String::new();
+    std::io::stdin().read_line(&mut input).unwrap();
+    input.trim().to_string()
 }
 
 fn print_menu() {
